3b1553ebfe92a13eef81402243ce507311eef78f
AtsuyaMorishita <mori.a0426@gmail.com>
2023-10-08 19:44:41 +0900

    githubとの連携

--- forge.config.js
+++ forge.config.js
@@ -1,29 +1,46 @@
+require("dotenv").config();
+
+process.env.GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
+
 module.exports = {
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: process.env.GITHUB_USER_NAME,
+          name: process.env.GITHUB_REPOSITORY_NAME,
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
   packagerConfig: {
     asar: true,
   },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
   ],
